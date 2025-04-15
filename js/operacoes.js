ebb1fe57eb388e1d5599409f1e7f5722c2ec1ff7
Czar-07 <anthonio.tony26sp@gmail.com>
2025-04-15 13:24:55 -0300

    Update operacoes.js

--- js/operacoes.js
+++ js/operacoes.js
@@ -1,10 +1,12 @@
 const expoente = document.querySelector(".expoente");
 const raiz = document.querySelector(".raiz");
 const log = document.querySelector(".log");
+//pegam elementos do HTML com as classes expoente, raiz, e log
 
 expoente.addEventListener("click", botaoe);
 raiz.addEventListener("click", botaor);
 log.addEventListener("click", botaol);
+//define o que acontece quando o usuário clica nos botões
 
 function botaoe() {
     var base = Number(prompt("Insira a base:"));
@@ -13,6 +15,7 @@ function botaoe() {
     alert("O resultado é: " + resultadoe);
     console.log(resultadoe)
 }
+//função para pedir dois valores no prompt, a base e o expoente, calcula usando "**" e o resultado aparece no alert e no console.
 
 function botaor() {
     var raiz = Number(prompt("Insira o número:"))
@@ -26,6 +29,8 @@ function botaor() {
    
 }
 
+//função para pedir um número e verifica se ele é maior ou igual a zero, calcula usando "** 0.5" e mostra o resultado no alert e no console.
+
 function botaol() {
     var numero = Number(prompt("Digite um número:"));
     var base = Number(prompt("Digite a base do logaritmo:"));
@@ -36,3 +41,4 @@ function botaol() {
         alert("Número ou base inválidos! O número e a base devem ser positivos e a base não pode ser 1.");
     }
 }
+//função para pedir um número e a base para o logaritmo, verificar os valores corretos e mostrar o resultado no alert e no console.
